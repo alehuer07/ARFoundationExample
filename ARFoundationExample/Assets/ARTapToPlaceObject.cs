34e00c62fa3c0bbd34aecff57d229daa8407c760
Alexis Huerta <alehuer90@gmail.com>
2019-06-06 10:13:31 -0700

    working build is complete, can now place objects

--- ARFoundationExample/Assets/ARTapToPlaceObject.cs
+++ ARFoundationExample/Assets/ARTapToPlaceObject.cs
@@ -7,15 +7,19 @@ using System;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
+
+    public GameObject objectToPlace;
     public GameObject placementIndicator;
 
-    private ARSessionOrigin arOrigin;
+    //private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRaycastMgr;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
     // Start is called before the first frame update
     void Start()
     {
-        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        //arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastMgr = FindObjectOfType<ARRaycastManager>();
     }
 
     // Update is called once per frame
@@ -23,8 +27,39 @@ public class ARTapToPlaceObject : MonoBehaviour
     {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
+
+        if(placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
     }
 
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    //Updates the spot where the object will be placed
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();    //the list of hits from the raycast
+        //arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+        arRaycastMgr.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    //Updates the position of the indicator and the visibility of it
     private void UpdatePlacementIndicator()
     {
         if(placementPoseIsValid)
@@ -38,17 +73,5 @@ public class ARTapToPlaceObject : MonoBehaviour
         }
     }
 
-    //Updates the spot where the object will be placed
-    private void UpdatePlacementPose()
-    {
-        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        var hits = new List<ARRaycastHit>();
-        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
-
-        placementPoseIsValid = hits.Count > 0;
-        if(placementPoseIsValid)
-        {
-            placementPose = hits[0].pose;
-        }
-    }
+    
 }
